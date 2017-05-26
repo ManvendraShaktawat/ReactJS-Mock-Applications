ae71cc786c2c6eab61fcfee60ee8eeebb9d87b3d
Manvendra Shaktawat <mshakt@sapient.com>
2017-05-27 01:24:30 +0530

    webpack altered

--- Pluralsight Administration - Flux + Webpack/src/main.jsx
+++ Pluralsight Administration - Flux + Webpack/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import routes from './routes.jsx';
 import InitializeActions from './actions/initializeActions';
+import './dependencies/header.css';
+import './dependencies/common.css';
 
 InitializeActions.initApp();
 ReactDOM.render(<BrowserRouter>{routes}</BrowserRouter>, document.getElementById('app'));
